b8dc0c20b6f05e717c77fdc2be66de4c981840a3
Yuta KTD <61367978+Yuta-KTD@users.noreply.github.com>
2023-05-04 05:28:04 +0900

    ログイン画面の追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:go_router/go_router.dart';
+import 'package:shopping_with_you/ui/screen/login_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -10,63 +12,62 @@ void main() async {
   runApp(const MyApp());
 }
 
+/// go_routerのルーティング設定
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const HomeScreen();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'singin',
+          builder: (BuildContext context, GoRouterState state) {
+            return LoginScreen();
+          },
+        ),
+      ],
+    ),
+  ],
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ふたりのお買い物',
+    return MaterialApp.router(
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routerConfig: _router,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('ふたりのお買い物'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'ログイン画面はこちら',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            ElevatedButton(
+              onPressed: () => context.go('/singin'),
+              child: const Text('ログイン画面'),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ lib/ui/component/form_text_field.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+class FormTextField extends StatelessWidget {
+  final String name;
+  final ValueChanged<String?> onChanged;
+  final TextInputType? keyboardType;
+  final List<String>? autofillHints;
+  final bool? obscureText;
+  const FormTextField({
+    super.key,
+    required this.name,
+    required this.onChanged,
+    this.keyboardType,
+    this.obscureText,
+    this.autofillHints,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FormBuilderTextField(
+      name: name,
+      onChanged: onChanged,
+      keyboardType: keyboardType ?? TextInputType.text,
+      autofillHints: autofillHints,
+      obscureText: obscureText ?? false,
+    );
+  }
+}

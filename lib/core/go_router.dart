189a867a739fe334d9ba1e3edeeed74b4383e57e
Yuta KTD <61367978+Yuta-KTD@users.noreply.github.com>
2023-06-24 11:10:57 +0900

    riverpodもつかってgo_routerを実装

---
+++ lib/core/go_router.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:shopping_with_you/view/screen/sign_in_screen.dart';
+import 'package:shopping_with_you/view/screen/sign_up_screen.dart';
+
+/// go_routerのルーティング設定
+final routerProvier = Provider((ref) => GoRouter(
+      routes: <RouteBase>[
+        GoRoute(
+          path: '/',
+          builder: (BuildContext context, GoRouterState state) {
+            return SignInSceen();
+          },
+          routes: <RouteBase>[
+            GoRoute(
+              path: 'signup',
+              builder: (BuildContext context, GoRouterState state) =>
+                  const SignUpScreen(),
+            )
+          ],
+        ),
+      ],
+    ));
